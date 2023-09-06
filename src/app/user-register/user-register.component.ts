7a2f10010415ef95600dbe1b2a23affe80515b7a
mateusquixada <mateusquixada@gmail.com>
2023-09-06 14:30:47 -0300

    feat: Cria instancias do formGropu no .ts

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
-export class UserRegisterComponent {
+export class UserRegisterComponent implements OnInit{
 
+  public form!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private fb:FormBuilder
+  ){}
+  
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required]
+    })
+  }
+
+  
 }
