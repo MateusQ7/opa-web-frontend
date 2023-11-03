4950d0f0cae82cb0e61c06accfe878e0bab7959f
CAUE <caue.ms01@gmail.com>
2023-11-03 20:01:42 -0300

    fix:merge problems

--- src/app/home/menu/menu.module.ts
+++ src/app/home/menu/menu.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu.component';
 import { YellowlineModule } from 'src/app/shared/yellowline/yellowline.module';
+import { CreateUpdateProductComponent } from './create-update-stock/create-update-stock.component';
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    CreateUpdateProductComponent,
   ],
   exports: [
     MenuComponent
