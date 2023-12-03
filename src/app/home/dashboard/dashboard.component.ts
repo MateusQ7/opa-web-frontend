da458c4800736d8617555b1f958066a5ab6baf04
Pedro Duarte <pcduarte@edu.unifor.br>
2023-12-03 12:22:46 -0300

    feat: Adiciona primeiro painel de faturamento

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import Chart, { ChartItem } from 'chart.js/auto'
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,9 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  public chart: any;
+  public perMonthRevenue: number[] = [];
+  public perMonthTicket: number[] = [];
 
   string = 'Dashboard';
 
@@ -14,7 +18,57 @@ export class DashboardComponent {
 
   ticket:string =`1.309,42`
 
+
   constructor(
     public auth:AuthService
   ){}
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    this.chart = new Chart('graph-target-member', {
+      type: 'bar',
+      data: {
+        labels: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
+        datasets: [
+          {
+            label: 'Ticket Médio em R$',
+            type: 'line',
+            data: [10, 20, 22, 20, 11, 15, 28, 31, 36, 40, 27, 21],
+            fill: false,
+            backgroundColor: '#956004',
+            borderColor: '#956004',
+            borderWidth: 2,
+            pointRadius: 2,
+            pointBackgroundColor: '#956004',
+          },
+          {
+            label: 'Faturamento em R$',
+            type: 'bar',
+            data: [10, 20, 15, 25, 22, 30, 28, 13, 2, 15, 17, 29],
+            backgroundColor: '#eec222',
+          }
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            beginAtZero: true
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Valor em R$'
+            }
+          }
+        }
+      }
+    })
+
+
+  }
 }
