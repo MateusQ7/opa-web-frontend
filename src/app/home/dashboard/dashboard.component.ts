af0c44e3522f6cf627ac02e3eaa7fefef1433b6c
Pedro Duarte <pcduarte@edu.unifor.br>
2023-12-03 14:34:43 -0300

    feat: Cria chart por faixa etária

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -8,7 +8,8 @@ import Chart, { ChartItem } from 'chart.js/auto'
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  public chart: any;
+  public revenueChart: any;
+  public memberChart: any;
   public perMonthRevenue: number[] = [];
   public perMonthTicket: number[] = [];
 
@@ -24,11 +25,12 @@ export class DashboardComponent {
   ){}
 
   ngOnInit(): void {
-    this.createChart();
+    this.createRevenueChart();
+    this.createMemberChart();
   }
 
-  createChart() {
-    this.chart = new Chart('graph-target-member', {
+  createRevenueChart() {
+    this.revenueChart = new Chart('graph-target-revenue', {
       type: 'bar',
       data: {
         labels: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
@@ -67,8 +69,62 @@ export class DashboardComponent {
           }
         }
       }
-    })
+    });
+  }
 
+  createMemberChart() {
+    this.memberChart = new Chart('graph-gender-member', {
+      type: 'bar',
+      data: {
+        labels: [
+          '0 - 20',
+          '21 - 25',
+          '26 - 30',
+          '31 - 35',
+          '36 - 40',
+          '41 - 45',
+          '46 - 50',
+          '51 - 55',
+          '56 - 60',
+          '61 - 65',
+          '66 - 70',
+          '70+',
+        ],
+        datasets: [
+          {
+            label: 'Mulher',
+            data: [10, 20, 15, 25, 22, 30, 28, 13, 2, 15, 17, 29],
+            backgroundColor: '#f1aeb5',
+          },
+          {
+            label: 'Homem',
+            data: [20, 40, 30, 50, 44, 60, 56, 26, 4, 30, 34, 58],
+            backgroundColor: '#9ec5fe',
 
+          },
+          {
+            label: 'Outros',
+            data: [15, 25, 20, 30, 27, 35, 33, 18, 7, 20, 22, 34],
+            backgroundColor: '#fff3cd',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        indexAxis: 'y',
+        scales: {
+          x: {
+            stacked: true,
+            display: false,
+          },
+          y: {
+            stacked: true,
+            title: {
+              display: true,
+            },
+          }
+        }
+      }
+    });
   }
 }
